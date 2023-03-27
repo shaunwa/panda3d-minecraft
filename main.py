4e37578f3a6fe28593109078ddc9264bdbd6a0c7
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-03-27 10:50:37 -0400

    Displayed three different blocks next to each other and made camera static (for now)

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-        block = loader.loadModel('dirt-block.glb')
-        block.reparentTo(render)
+        dirtBlock = loader.loadModel('dirt-block.glb')
+        dirtBlock.reparentTo(render)
+        dirtBlock.setHpr(0, 90, 0)
+
+        stoneBlock = loader.loadModel('stone-block.glb')
+        stoneBlock.reparentTo(render)
+        stoneBlock.setPos(0, 2, 0)
+        stoneBlock.setHpr(0, 90, 0)
+
+        grassBlock = loader.loadModel('grass-block.glb')
+        grassBlock.reparentTo(render)
+        grassBlock.setPos(0, -2, 0)
+        grassBlock.setHpr(0, 90, 0)
+
+        dirtBlock = loader.loadModel('dirt-block.glb')
+        dirtBlock.reparentTo(render)
 
         mainLight = DirectionalLight('main light')
         mainLightNodePath = render.attachNewNode(mainLight)
@@ -17,6 +31,10 @@ class Game(ShowBase):
         ambientLight.setColor((0.4, 0.4, 0.4, 1))
         ambientLightNodePath = render.attachNewNode(ambientLight)
         render.setLight(ambientLightNodePath)
+
+        self.disableMouse()
+        self.camera.setPos(10, -10, 10)
+        self.camera.lookAt(0, 0, 0)
 
 game = Game()
 game.run()
