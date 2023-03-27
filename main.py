368148676b22b31ffe7f53386d00f4ec4a5d02d6
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-03-27 12:44:34 -0400

    Separated code into helper methods

--- main.py
+++ main.py
@@ -5,20 +5,45 @@ class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-        dirtBlock = loader.loadModel('dirt-block.glb')
-        dirtBlock.reparentTo(render)
-        dirtBlock.setHpr(0, 90, 0)
+        self.loadModels()
+        self.setupLights()
+        self.setupCamera()
+        self.generateWorld()
 
-        stoneBlock = loader.loadModel('stone-block.glb')
-        stoneBlock.reparentTo(render)
-        stoneBlock.setPos(0, 2, 0)
-        stoneBlock.setHpr(0, 90, 0)
+    def setupCamera(self):
+        self.disableMouse()
+        self.camera.setPos(50, -50, 50)
+        self.camera.lookAt(0, 0, 0)
 
-        grassBlock = loader.loadModel('grass-block.glb')
-        grassBlock.reparentTo(render)
-        grassBlock.setPos(0, -2, 0)
-        grassBlock.setHpr(0, 90, 0)
+    def loadModels(self):
+        self.dirtBlock = loader.loadModel('dirt-block.glb')
+        self.dirtBlock.setHpr(0, 90, 0)
 
+        self.stoneBlock = loader.loadModel('stone-block.glb')
+        self.stoneBlock.setHpr(0, 90, 0)
+
+        self.grassBlock = loader.loadModel('grass-block.glb')
+        self.grassBlock.setHpr(0, 90, 0)
+
+    def generateWorld(self):
+        for z in range(10):
+            for x in range(20):
+                for y in range(20):
+                    newBlockNode = render.attachNewNode("new-box-placeholder")
+
+                    newBlockNode.setPos(
+                        x * 2 - 20,
+                        y * 2 - 20,
+                        -z * 2,
+                    )
+
+                    if z == 0:
+                        self.grassBlock.instanceTo(newBlockNode)
+                    else:
+                        self.dirtBlock.instanceTo(newBlockNode)
+
+
+    def setupLights(self):
         mainLight = DirectionalLight('main light')
         mainLightNodePath = render.attachNewNode(mainLight)
         mainLightNodePath.setHpr(30, -60, 0)
@@ -29,9 +54,6 @@ class Game(ShowBase):
         ambientLightNodePath = render.attachNewNode(ambientLight)
         render.setLight(ambientLightNodePath)
 
-        self.disableMouse()
-        self.camera.setPos(10, -10, 10)
-        self.camera.lookAt(0, 0, 0)
 
 game = Game()
 game.run()
