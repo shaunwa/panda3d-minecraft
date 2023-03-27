b3969f333f3013f7d401611bde17daf0472665d9
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-03-27 13:26:48 -0400

    Added player movement controls

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import DirectionalLight, AmbientLight
 from panda3d.core import WindowProperties
 from panda3d.core import TransparencyAttrib
 from direct.gui.DirectGui import *
 from direct.gui.OnscreenImage import OnscreenImage 
+
+def degToRad(degrees):
+    return degrees * (pi / 180.0)
 
 class Game(ShowBase):
     def __init__(self):
@@ -22,12 +27,46 @@ class Game(ShowBase):
     def update(self, task):
         dt = globalClock.getDt()
 
+        self.movePlayer(dt)
+
         if self.cameraSwingActivated:
             self.moveCameraWithMouse(dt)
 
         return task.cont
 
+    def movePlayer(self, dt):
+        playerMoveSpeed = 10
+
+        x_movement = 0
+        y_movement = 0
+        z_movement = 0
+
+        if self.keyMap['forward']:
+            x_movement -= dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+            y_movement += dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+        if self.keyMap['backward']:
+            x_movement += dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+            y_movement -= dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+        if self.keyMap['left']:
+            x_movement -= dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+            y_movement -= dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+        if self.keyMap['right']:
+            x_movement += dt * playerMoveSpeed * cos(degToRad(camera.getH()))
+            y_movement += dt * playerMoveSpeed * sin(degToRad(camera.getH()))
+        if self.keyMap['up']:
+            z_movement += dt * playerMoveSpeed
+        if self.keyMap['down']:
+            z_movement -= dt * playerMoveSpeed
+
+        camera.setPos(
+            camera.getX() + x_movement,
+            camera.getY() + y_movement,
+            camera.getZ() + z_movement,
+        )
+
     def moveCameraWithMouse(self, dt):
+        cameraSwingFactor = 10
+
         md = self.win.getPointer(0)
         mouseX = md.getX()
         mouseY = md.getY()
@@ -38,8 +77,8 @@ class Game(ShowBase):
         currentP = self.camera.getP()
 
         self.camera.setHpr(
-            currentH - mouseChangeX * dt * self.cameraSwingFactor,
-            min(90, max(-90, currentP - mouseChangeY * dt * self.cameraSwingFactor)),
+            currentH - mouseChangeX * dt * cameraSwingFactor,
+            min(90, max(-90, currentP - mouseChangeY * dt * cameraSwingFactor)),
             0
         )
 
@@ -52,8 +91,6 @@ class Game(ShowBase):
     def setupCamera(self):
         self.disableMouse()
 
-        self.cameraSwingFactor = 10
-
         self.camera.setPos(0, 0, 3)
         self.camLens.setFov(80)
 
@@ -63,7 +100,32 @@ class Game(ShowBase):
         icon.setTransparency(TransparencyAttrib.MAlpha)
     
     def setupControls(self):
+        self.keyMap = {
+            "forward" : False,
+            "backward" : False,
+            "left" : False,
+            "right" : False,
+            "up" : False,
+            "down" : False,
+        }
+
+        self.accept("w", self.updateKeyMap, ["forward", True])
+        self.accept("w-up", self.updateKeyMap, ["forward", False])
+        self.accept("s", self.updateKeyMap, ["backward", True])
+        self.accept("s-up", self.updateKeyMap, ["backward", False])
+        self.accept("a", self.updateKeyMap, ["left", True])
+        self.accept("a-up", self.updateKeyMap, ["left", False])
+        self.accept("d", self.updateKeyMap, ["right", True])
+        self.accept("d-up", self.updateKeyMap, ["right", False])
+        self.accept("space", self.updateKeyMap, ["up", True])
+        self.accept("space-up", self.updateKeyMap, ["up", False])
+        self.accept("lshift", self.updateKeyMap, ["down", True])
+        self.accept("lshift-up", self.updateKeyMap, ["down", False])
+
         self.accept('escape', self.releaseMouse)
+    
+    def updateKeyMap(self, controlName, controlState):
+        self.keyMap[controlName] = controlState
     
     def captureMouse(self):
         md = self.win.getPointer( 0 )
